56291dda128c3044e4833a1cad7ec072b8617a4e
Oguzhan <oguzhanzny@gmail.com>
2023-09-25 03:42:56 +0300

    css template for app completed

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,7 @@ import "./app.css";
 function App() {
   return (
     <div className="App">
-      <div className="gradient_bg">
+      <div className="gradient__bg">
         <Navbar />
         <Header />
       </div>
